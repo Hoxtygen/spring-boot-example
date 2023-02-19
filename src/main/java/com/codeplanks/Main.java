62a898a532aca10bc778d2b576ef46cc971d2e1c
Wasiu Idowu <hoxtygen@live.com>
2023-02-19 12:21:36 +0100

    Add sample GET request

--- src/main/java/com/codeplanks/Main.java
+++ src/main/java/com/codeplanks/Main.java
@@ -1,2 +1,34 @@
-package com.codeplanks;public class Main {
+package com.codeplanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+
+  record Person(String name, int age, double savings) {
+  }
+
+  record GreetResponse(String greet, List<String> favProgrammingLangs,
+                       Person person) {
+  }
+
+  @GetMapping("/")
+  public GreetResponse greet() {
+    GreetResponse response = new GreetResponse("Hello",
+            List.of("Python", "Java", "JavaScript", "Perl"),
+            new Person("Mahone", 36, 25_000)
+    );
+    return response;
+
+  }
+
 }
