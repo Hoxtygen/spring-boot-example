3f7f5cea4304f42ce5a816d265c92d948425aa3a
Wasiu Idowu <hoxtygen@live.com>
2023-02-19 18:52:25 +0100

    Add customer repository

--- src/main/java/com/codeplanks/CustomerRepository.java
+++ src/main/java/com/codeplanks/CustomerRepository.java
@@ -1,2 +1,6 @@
-package com.codeplanks;public interface CustomerRepository {
+package com.codeplanks;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
